829e42f861fc4affc45349fca03093ebce806cf7
Sean Mitchell <sm0077@uah.edu>
2019-02-11 13:49:11 -0600

    Added TODO.txt, Player class

---
+++ src/Player_Class.java
@@ -0,0 +1,33 @@
+public class Player
+{
+	// Member Variables
+	private int location;
+	private int money;
+	private int[] property_list;
+	// token
+	private String name;
+	
+	// Default constructor
+	public Player()
+	{
+
+	}
+
+	// Accessor Functions
+	public int Get_Location()
+	{
+		// Returns location as an int
+	}
+
+	public int Get_Money()
+	{
+		// Returns current player money as an int
+	}
+
+	public String Get_Name()
+	{
+		// Returns player's name as a string
+	}
+	
+	// Mutator Functions
+}
