5ab82af99aa2711708edb7b9e1d10b110ff0bb31
M-azing1 <47499509+M-azing1@users.noreply.github.com>
2019-03-01 03:32:47 -0600

    Update Property.java
    Class filling for Property class

--- src/Property.java
+++ src/Property.java
@@ -4,18 +4,62 @@
 
 // Imports
 
-public class Property
-{
+public class Property{
+
 	// Member Variables
+
 	private int location;
 	private int cost;
 	private String owner;
 	// token, this will be an image file
 	private String name;
-	
-	// Default constructor
-	public Property()
-	{
 
-	}
+    // Default constructor
+        public Property(){      
+        }
+        public Property(int location, int cost, String owner, String name){
+            Set_Location(location);
+            Set_Cost(cost);
+            Set_Owner(owner);
+            Set_Name(name);  
+        }
+        
+    //Accessor Methods
+        
+        public int Get_Loacation(){
+            return location;
+        }
+        
+        public int Get_Cost(){
+            return cost;
+        }
+        
+        public String Get_Owner(){
+            return owner;
+        }
+        
+        public String Get_Name(){
+            return name;
+        }
+        
+        
+        //Mutator Methods
+        
+        //throw negative number exception
+        public void Set_Location(int x){
+            if(x < 0){
+                throw new NegativeException("Can't set location to negative number");
+            }
+            location = x;
+        }
+        public void Set_Owner(String newOwner){
+            owner = newOwner;
+        }
+        public void Set_Cost(int newCost){
+            cost = newCost;
+        }
+        public void Set_Name(String newName){
+            name = newName;
+        }
+
 }
