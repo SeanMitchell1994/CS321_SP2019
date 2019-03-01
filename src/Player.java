8587f431fd1717901e14b5e68ecea9219d442594
M-azing1 <47499509+M-azing1@users.noreply.github.com>
2019-03-01 03:06:37 -0600

    Update Player.java
    A little class filling. Added Shift_Location and Shift_Money functions as well;

--- src/Player.java
+++ src/Player.java
@@ -6,64 +6,103 @@
 // Imports
 
 public class Player
+
 {
+
 	// Member Variables
+
 	private int location;
+
 	private int money;
+
 	private int[] property_list;
+
 	// token, this will be an image file
+
 	private String name;
+
 	private boolean is_ready;
+
 	
+
 	// Default constructor
-	public Player()
-	{
+
+	public Player(){
+
+
 
 	}
 
+
+
 	// ==================================
+
 	// Accessor Functions
+
 	// ==================================
-	public int Get_Location()
-	{
-		// Returns location as an int
+// Returns location as an int
+	public int Get_Location(){
+                return location;
+	}
+// Returns current player money as an int
+	public int Get_Money(){
+               return money;
+	}
+        // Returns player's name as a string
+	public String Get_Name(){
+            return name;
 	}
 
-	public int Get_Money()
-	{
-		// Returns current player money as an int
+// returns if the player is done with their turn
+	public boolean Is_Ready(){
+            return is_ready;
 	}
 
-	public String Get_Name()
-	{
-		// Returns player's name as a string
-	}
-
-	public boolean Is_Ready()
-	{
-		// returns if the player is done with their turn
-	}
 	
+
 	// ==================================
+
 	// Mutator Functions
+
 	// ==================================
-	public void Set_Location(int input)
-	{
-		// sets the player's location as an int
+        
+        // sets the player's location as an int
+	public void Set_Location(int input){
+            location = input;
+	}
+        // changes a players location based on a value passed in
+        public void Shift_Location(int shift){
+            if((location + shift) > 39){
+                Set_Location((location+shift) - 40);
+            }
+            else if((location + shift) < 0){
+                Set_Location(40+(location+shift));
+            }
+            else{
+                Set_Location(location+shift);
+            }
+        }
+
+        // sets the player's money
+	public void Set_Money(int input){
+        money = input;
+	}
+        
+        //shifts a player's money (add/subtraction)
+        //needs an if to call morgage/forfiet option when money reaches below zero
+        public void shift_Money(int input){
+            money = money + input;
+        }
+
+// sets the player's name
+	public void Set_Name(String input){
+            name = input;
 	}
 
-	public void Set_Money(int input)
-	{
-		// sets the player's money
-	}	
-
-	public void Set_Name(String input)
-	{
-		// sets the player's name
-	}
 	
-	public void Set_Ready(boolean input)
-	{
-		// Set if the player is done with their turn
+// Set if the player is done with their turn
+	public void Set_Ready(boolean input){
+            is_ready = input;
 	}
+
 }
