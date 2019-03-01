120543c07ebd9e33c763e5e52b390871806df5a2
M-azing1 <47499509+M-azing1@users.noreply.github.com>
2019-03-01 06:05:29 -0600

    Update Property.java
    added ImageIcon variable and corresponding getter/setter functions

--- src/Property.java
+++ src/Property.java
@@ -4,6 +4,9 @@
 
 // Imports
 
+
+import javax.swing.ImageIcon;
+
 public class Property{
 
 	// Member Variables
@@ -11,13 +14,15 @@ public class Property{
 	private int location;
 	private int cost;
 	private String owner;
+        private ImageIcon image;
 	// token, this will be an image file
 	private String name;
 
     // Default constructor
-        public Property(){      
-        }
-        public Property(int location, int cost, String owner, String name){
+        public Property(){
+            
+	}
+        public Property(int location, int cost, String owner, String name, ImageIcon image){
             Set_Location(location);
             Set_Cost(cost);
             Set_Owner(owner);
@@ -42,24 +47,45 @@ public class Property{
             return name;
         }
         
+        public ImageIcon Get_Image(){
+            return image;
+        }
+        
         
         //Mutator Methods
         
-        //throw negative number exception
         public void Set_Location(int x){
+
             if(x < 0){
+
                 throw new NegativeException("Can't set location to negative number");
+
             }
+
             location = x;
+
         }
+
         public void Set_Owner(String newOwner){
+
             owner = newOwner;
+
         }
+
         public void Set_Cost(int newCost){
+
             cost = newCost;
+
         }
+
         public void Set_Name(String newName){
+
             name = newName;
+
+        }
+        
+        public void Set_Image(ImageIcon x){
+            image = x;
         }
 
 }
