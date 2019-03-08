cd3ef59a62d36ad331a3e6c2d3f5b8a83c0bdb7f
ansleysolo <33235644+ansleysolo@users.noreply.github.com>
2019-03-07 20:46:53 -0600

    Create Utilities.java

---
+++ src/Utilities.java
@@ -0,0 +1,25 @@
+// ========================================
+// Utilities.java
+// Main class for the utilities objects (subset of properties)
+// ========================================
+
+// Imports
+
+import javax.swing.ImageIcon;
+
+public class Utilities
+{
+
+	// Member variables read in from file
+	private String name;
+	private int location;
+  private int price;
+  private int rent1;
+  private int rent2;
+  private int mortgageValue;
+	
+  // Member variables that are initialized in the game
+	private String owner;
+  private int currentRent; 
+  
+}
