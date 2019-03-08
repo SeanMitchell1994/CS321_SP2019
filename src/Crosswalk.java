88244f948e29562039eea9f96476ee5c659b3620
ansleysolo <33235644+ansleysolo@users.noreply.github.com>
2019-03-07 20:44:12 -0600

    Create Crosswalk.java
    Added variables needed for crosswalk property class

---
+++ src/Crosswalk.java
@@ -0,0 +1,26 @@
+// ========================================
+// Crosswalk.java
+// Main class for the crosswalk objects (subset of properties)
+// ========================================
+
+// Imports
+
+import javax.swing.ImageIcon;
+
+public class Crosswalk
+{
+
+	// Member variables read in from file
+	private String name;
+	private int location;
+  private int price;
+  private int rent1;
+  private int rent2;
+  private int rent3;
+  private int rent4;
+	
+  // Member variables that are initialized in the game
+	private String owner;
+  private int currentRent; 
+  
+}
