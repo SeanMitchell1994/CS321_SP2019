fa7481aa0db4495a33895bb423cc1ba015c290cf
M-azing1 <47499509+M-azing1@users.noreply.github.com>
2019-03-14 06:58:26 -0500

    Update Card_Factory.java
    added some comments for javaDoc

--- src/Card_Factory.java
+++ src/Card_Factory.java
@@ -18,7 +18,13 @@ public class Card_Factory
 		
 	}
 
-	// Public accessor to the singleton instance
+	
+    /*
+    // Author: Sean Mitchel
+    // public static Card_Factory() Get_Instance()
+    // Public accessor to the singleton instance
+    // 
+    */
 	public static Card_Factory() Get_Instance()
 	{
 		if(instance == null)
@@ -28,17 +34,35 @@ public class Card_Factory
 
 		return instance;
 	}
-
+ 
+     /*
+    // Author: Sean Mitchel
+    // Create_Deck(String type, int number)
+    // Returns a list of cards. Number of cards is sepecified by int "number" passed in, and type of cards 
+    // is specified by the string 'type" passed in
+    */
 	public Card Create_Deck(String type, int number)
 	{
 		// Returns a list of cards
 	}
 
+    /*
+    // Author: Sean Mitchel
+    // Create_Chance_Card()
+    // Creates a Card with type "chance". Card information is pulled from an XML sheet.
+    // 
+    */
 	private Card Create_Chance_Card()
 	{
 		// Creates a chance card
 	}
 
+    /*
+    // Author: Sean Mitchel
+    // Create_Community_Card()
+    // Creates a Card with type "community". Card information is pulled from an XML sheet.
+    // 
+    */
 	private Card Create_Community_Card()
 	{
 		// Creates a community card
