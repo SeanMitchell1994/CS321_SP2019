f8aa88bc3680957cc2bbc17c3447572ead362515
M-azing1 <47499509+M-azing1@users.noreply.github.com>
2019-03-14 07:41:13 -0500

    Update Dice_Roller.java
    added comments for javaDoc

--- src/Dice_Roller.java
+++ src/Dice_Roller.java
@@ -5,18 +5,31 @@
 // Imports
 import java.util.Random;
 
+//WILL WE RECOGNIZE DOUBLES ROLLED?
 public class Dice_Roller
 {
 	private final Random rand_int;
 	private final int sides;
 
-	// Default constructor
+/*
+// Author: Sean Mitchel
+// Dice_Roller(int sides)  
+// Default Dice_Roller constructor. rand_int is set to a new random and sides is set to the value passed in.
+// 
+*/
 	public Dice_Roller(int sides)
 	{
 		this.rand_int = new Random();
 		this.sides = sides;
 	}
 
+
+/*
+// Author: Sean Mitchel
+// Roll()  
+// Rolls the dice and returns the result as an int
+// 
+*/	
 	// Rolls the dice and returns the result as an int
 	public int Roll()
 	{
