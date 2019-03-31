d5a0f6f5497765b88709a3ebd69b25e8c14e32f3
ansleysolo <33235644+ansleysolo@users.noreply.github.com>
2019-03-30 20:07:21 -0500

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -3,54 +3,67 @@
 // ========================================
 
 // Imports
-import Card_Event.java;
+//import Card_Event.java;
 
 public class Card
 {
 	private final String card_type;
-	private final int card_id;
-	private final Card_Event event;
+	private final String card_id;
+	//private final Card_Event event;
 	private final String text;
 	//private final token
 
      /*
-    // Author: Sean Mitchel
+    // Author: Sean Mitchell, Ansley Solomon
     // Card(int event_input, int card_id_input)
-    // Default construct for a card object. A card's event is set to even_input passed in and
+    // Default construct for a card object. A card's event is set to event_input passed in and
     // card_id is set to card_id_input passed in.
     */
-	public Card(int event_input, int card_id_input, String text_input)
+	public Card(String card_id_input, String text_input, String type_input)
 	{
-		event = event_input;
+		//event = event_input;
 		card_id = card_id_input;
 		text = text_input;
+                card_type = type_input;
 	}
 
     /*
-    // Author: Sean Mitchel
-    // Get_Card_Typre
+    // Author: Ansley Solomon
+    // getCardType()
     // Returns a card's type as a string.
-    // Useful for determining if a card is chance or community
     */
-	public String Get_Card_Type()
+	public String getCardType()
 	{
-		// TODO: Move this to constructor
-		str = "";
-		if (card_type == 1) {str = "chance";}
-		else {str = "community";}
-		return str;
+		return card_type;
 	}
-	
+        
+        // Author: Ansley Solomon
+        // getID()
+        // Returns a card's ID number as an int
+        public int getID()
+        {
+            int id = Integer.parseInt(card_id);
+            return id;
+        }
+        
+        // Author: Ansley Solomon
+        // getText()
+        // Returns a card's text as a string
+        public String getText(){
+            return text;
+        }
+        
      /*
     // Author: Sean Mitchel
     // Get_Event
     // Returns the event string from the member event object in a card
     /
-    */
+    
 	public Get_Event()
 	{
 		// Returns the event string from the member even object
 		// TODO: add the event integer 
 		return this.event.Get_Event();
 	}
+        */
 }
