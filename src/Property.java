15032aaf93efa7c2ddc052e84c64c8291d3e50d1
ansleysolo <33235644+ansleysolo@users.noreply.github.com>
2019-04-04 16:20:16 -0500

    Update Property.java

--- src/Property.java
+++ src/Property.java
@@ -12,31 +12,45 @@ public class Property
 {
 
 	// Member Variables
-	private String name;
-	private int location;
-	private String color;
-	private int price;
-	private int mortgageValue;
-	private int buildingCost;
-	private int rent;           // Cost of rent with no monopoly or buildings
-	private int monopolyRent;   // Cost of rent when owner has monopoly
-	private int rentHouse1;     // Cost of rent with one house
-	private int rentHouse2;     // Cost of rent with two houses
-	private int rentHouse3;     // Cost of rent with three houses
-	private int rentHouse4;     // Cost of rent with four houses
-	private int rentHotel;      // Cost of rent with hotel
+	private final String name;
+	private final String location;
+	private final String color;
+	private final String price;
+	private final String mortgageValue;
+	private final String buildingCost;
+	private final String rent;           // Cost of rent with no monopoly or buildings
+	private final String monopolyRent;   // Cost of rent when owner has monopoly
+	private final String rentHouse1;     // Cost of rent with one house
+	private final String rentHouse2;     // Cost of rent with two houses
+	private final String rentHouse3;     // Cost of rent with three houses
+	private final String rentHouse4;     // Cost of rent with four houses
+	private final String rentHotel;      // Cost of rent with hotel
 	
 	private String owner;
         private ImageIcon image;
 
+        // Author: Ansley Solomon
     	// Default constructor
-        public Property(int location_input)
+        public Property(String pName, String pLocation, String pColor, String pPrice, 
+                String pMV, String pBC, String pRent, String pMR, String pRent1, 
+                String pRent2, String pRent3, String pRent4, String pRentH)
 	{
-            	Set_Location(location_input);
-		System.out.printf("Property created at location %d!\n", this.Get_Location());
-		this.price = 50;
+            name = pName;
+            location = pLocation;
+            color = pColor;
+            price = pPrice;
+            mortgageValue = pMV;
+            buildingCost = pBC;
+            rent = pRent;
+            monopolyRent = pMR;
+            rentHouse1 = pRent1;
+            rentHouse2 = pRent2;
+            rentHouse3 = pRent3;
+            rentHouse4 = pRent4;
+            rentHotel = pRentH;
 	}
 
+        /*
 	// overloaded constructor
         public Property(int location, int cost, String owner, String name, ImageIcon image)
 	{
@@ -45,71 +59,70 @@ public class Property
             Set_Owner(owner);
             Set_Name(name);  
         }
+        */
         
     	//Accessor Methods
         
-        public int Get_Location(){
-            return location;
-        }
-        
-        public int Get_Cost(){
-            return price;
-        }
-        
-        public String Get_Owner(){
-            return owner;
-        }
-        
-        public String Get_Name(){
+        public String getName(){
             return name;
         }
         
-        public ImageIcon Get_Image(){
-            return image;
+        public int getLocation(){
+            int lo = Integer.parseInt(location);	
+            return lo;
         }
         
-        
-        //Mutator Methods
-        
-	// temporarly commented for debuging
-        /*public void Set_Location(int x){
-
-            if(x < 0){
-
-                throw new NegativeException("Can't set location to negative number");
-
-            }
-
-            location = x;
-
-        }*/
-
-      	public void Set_Location(int location_input){
-
-            location = location_input;
-
-        }
-
-        public void Set_Owner(String newOwner){
-
-            owner = newOwner;
-
-        }
-
-        public void Set_Cost(int newCost){
-
-            price = newCost;
-
-        }
-
-        public void Set_Name(String newName){
-
-            name = newName;
-
+        public int getPrice(){
+            int p = Integer.parseInt(price);
+            return p;
         }
         
-        public void Set_Image(ImageIcon x){
-            image = x;
+        public String getColor(){
+            return color;
         }
-
+        
+        public int getMortgageValue(){
+            int mv = Integer.parseInt(mortgageValue);
+            return mv;
+        }
+        
+        public int getBuildingCost(){
+            int bc = Integer.parseInt(buildingCost);
+            return bc;
+        }
+        
+        public int getRent(){
+            int r = Integer.parseInt(rent);
+            return r;
+        }
+        
+        public int getMonopolyRent(){
+            int mr = Integer.parseInt(monopolyRent);
+            return mr;
+        }
+        
+        public int getRent1(){
+            int r = Integer.parseInt(rentHouse1);
+            return r;
+        }
+        
+        public int getRent2(){
+            int r = Integer.parseInt(rentHouse2);
+            return r;
+        }
+        
+        public int getRent3(){
+            int r = Integer.parseInt(rentHouse3);
+            return r;
+        }
+        
+        public int getRent4(){
+            int r = Integer.parseInt(rentHouse4);
+            return r;
+        }
+        
+        public int getRentHotel(){
+            int r = Integer.parseInt(rentHotel);
+            return r;
+        }
 }
