43bc9276c48910b44e737b074c9be7ef589770e8
Sean <sm0077@uah.edu>
2019-04-09 21:11:14 -0500

    Updated documentation

--- src/Tile_Adapter.java
+++ src/Tile_Adapter.java
@@ -1,33 +1,61 @@
+/**
+ * Tile_Adapter.java
+ * This class contains the adapters to allow the various property classes 
+ * to interact with a common set of code
+ * 
+ * @author Sean Mitchell
+ */
 
 import java.util.ArrayList;
 
-/**
- *
- * @author Sean
- */
 public class Tile_Adapter 
 {
-    Property local_p;
-    Crosswalk local_cw;
-    Utility local_u;
-    int flag;
-    boolean is_owned;
-    String owner = "None";
+    // ======================================
+    // Member variables
+    // ======================================
+    private Property local_p;
+    private Crosswalk local_cw;
+    private Utility local_u;
+    
+    private int flag;
+    private boolean is_owned;
+    private String owner = "None";
         
+    // ======================================
+    // Constructors
+    // ======================================
+    /**
+    * Constructor for Property types
+    *
+    * @param                    None
+    * @return   Tile_Adapter    Tile_Adapter object
+    */
     public Tile_Adapter(Property input)
     {
         //System.out.println("Tile object  created!");
         this.local_p = input;
         this.flag = 1;
     }
-        
+       
+    /**
+    * Constructor for Crosswalk types
+    *
+    * @param                    None
+    * @return   Tile_Adapter    Tile_Adapter object
+    */
     public Tile_Adapter(Crosswalk input)
     {
         //System.out.println("CW object  created!");
         this.local_cw = input;
         this.flag = 2;
     }
-            
+         
+    /**
+    * Constructor for Utility types
+    *
+    * @param                    None
+    * @return   Tile_Adapter    Tile_Adapter object
+    */
     public Tile_Adapter(Utility input)
     {
         //System.out.println("Utlity object created!");
@@ -35,6 +63,16 @@ public class Tile_Adapter
         this.flag = 3;
     }
     
+    // ======================================
+    // Mutator functions
+    // ======================================
+    
+    /**
+    * Adapter interface for setting if a tile's owner
+    *
+    * @param    String  input
+    * @return           None
+    */
     public void Set_Owner(String input)
     {
         if (flag == 1)
@@ -44,6 +82,12 @@ public class Tile_Adapter
         return;
     }
     
+    /**
+    * Adapter interface for setting if a tile is owned
+    *
+    * @param    input   boolean value
+    * @return           None
+    */
     public void Set_IsOwned(boolean input)
     {
         if ((this.flag == 1 ) || (this.flag == 2))
@@ -52,16 +96,37 @@ public class Tile_Adapter
             this.is_owned = false;
     }
     
+    // ======================================
+    // Accessor functions
+    // ======================================
+    /**
+    * Adapter interface for is tile classification
+    *
+    * @param            None
+    * @return   int     Tile classification
+    */
     public int getType()
     {
         return flag;
     }
     
+    /**
+    * Adapter interface for is tile currently owned
+    *
+    * @param            None
+    * @return   boolean Is tile owned?
+    */
     public boolean Get_IsOwned()
     {
         return this.is_owned;
     }
     
+    /**
+    * Adapter interface for tile names
+    *
+    * @param            None
+    * @return   String  Name of tile
+    */
     public String getName()
     {
         //System.out.print("debug!\n");
@@ -76,6 +141,12 @@ public class Tile_Adapter
         return temp;
     }
     
+    /**
+    * Adapter interface for tile locations
+    *
+    * @param            None
+    * @return   int     Location of tile
+    */
     public int getLocation()
     {
         int temp;
@@ -89,6 +160,12 @@ public class Tile_Adapter
         return temp;
     }
 
+    /**
+    * Adapter interface for tile prices
+    *
+    * @param            None
+    * @return   int     Price of tile
+    */
     public int getPrice()
     {
         int temp;
@@ -101,5 +178,4 @@ public class Tile_Adapter
         
         return temp;
     }
-
 }
