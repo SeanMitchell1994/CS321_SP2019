6995a45f1fee64a6e29a29e364d6927a72be6940
ansleysolo <33235644+ansleysolo@users.noreply.github.com>
2019-04-12 19:29:54 -0500

    Added javadoc comments

--- src/Dice_Roller.java
+++ src/Dice_Roller.java
@@ -1,38 +1,36 @@
-// ========================================
-// Dice_Roller.java
-// ========================================
+package monopolydriver;
 
-// Imports
+/**
+ * Dice_Roller class
+ * 
+ * This class contains the constructor for the Dice_Roller object. 
+ * 
+ * @author Sean Mitchell
+ */
 import java.util.Random;
+//call twice for two die
 
-//call twice fpr two die
-public class Dice_Roller
-{
-	private final Random rand_int;
-	private final int sides;
+public class Dice_Roller{
+    private final Random rand_int;
+    private final int sides;
+        
+    /**
+     * Default constructor
+     * Creates a random integer
+     * @author Sean Mitchell
+     * @param i Number of sides on the die
+     */
+    Dice_Roller(int i) {
+     this.rand_int = new Random();
+     this.sides = i;
+    }
 
-/*
-// Author: Sean Mitchel
-// Dice_Roller(int sides)  
-// Default Dice_Roller constructor. rand_int is set to a new random and sides is set to the value passed in.
-// 
-*/
-	public Dice_Roller(int sides)
-	{
-		this.rand_int = new Random();
-		this.sides = sides;
-	}
-
-
-/*
-// Author: Sean Mitchel
-// Roll()  
-// Rolls the dice and returns the result as an int
-// 
-*/	
-	// Rolls the dice and returns the result as an int
-	public int Roll()
-	{
-		return 1 + rand_int.nextInt(sides);
-	}
-}	
+    /**
+     * Rolls a die and returns the result as an int
+     * @author Sean Mitchell
+     * @return Random number between 1 and the number of sides
+     */
+    public int Roll(){
+        return 1 + rand_int.nextInt(sides);
+    }
+}
