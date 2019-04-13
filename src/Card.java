e23a052a457953ed7cae895d68e4ae3d027be355
ansleysolo <33235644+ansleysolo@users.noreply.github.com>
2019-04-12 19:25:15 -0500

    Update Card.java
    Added javadoc comments

--- src/Card.java
+++ src/Card.java
@@ -1,10 +1,13 @@
-// ========================================
-// Card.java
-// ========================================
+package monopolydriver;
 
-// Imports
-//import Card_Event.java;
-
+/**
+ * Card class
+ * 
+ * This class contains the card object and methods to access the individual
+ * card components. 
+ * 
+ * @author Ansley Solomon, Sean Mitchell
+ */
 public class Card
 {
 	private final String card_type;
@@ -13,14 +16,18 @@ public class Card
 	private final String text;
         private final String event_type;
         private final String event_value;
-	//private final token
-
-     /*
-    // Author: Sean Mitchell, Ansley Solomon
-    // Card(int event_input, int card_id_input)
-    // Default construct for a card object. A card's event is set to event_input passed in and
-    // card_id is set to card_id_input passed in.
-    */
+    
+    /*
+     * @author Sean Mitchell, Ansley Solomon
+     * Default construct for a card object
+     *
+     * @param card_id_input The card's ID number
+     * @param text_input The card's text
+     * @param type_input The card's type (chance or charger chest)
+     * @param event_type_input The type of event the card performs
+     * @param event_value_input The value that goes in the even function
+     * @return None
+     */
 	public Card(String card_id_input, String text_input, String type_input, String event_type_input, String event_value_input)
 	{
 		//event = event_input;
@@ -32,46 +39,61 @@ public class Card
 	}
 
     /*
-    // Author: Ansley Solomon
-    // getCardType()
-    // Returns a card's type as a string.
-    */
+     * This method returns the card's type (chance or charger chest)
+     * @author Ansley Solomon
+     * @return Returns the card's type (chance or charger chest)
+     */
     public String getCardType()
     {
             return card_type;
     }
-
-    // Author: Ansley Solomon
-    // getID()
-    // Returns a card's ID number as an int
+    
+    /*
+     * This method returns the card's ID number
+     * @author Ansley Solomon
+     * @return Returns the card's ID number as an int
+     */
     public int getID()
     {
         int id = Integer.parseInt(card_id);
         return id;
     }
 
-    // Author: Ansley Solomon
-    // getText()
-    // Returns a card's text as a string
+    /*
+     *This method returns the card's text as a string
+     * @author Ansley Solomon
+     * @return Returns the card's text as a string
+     */   
     public String getText(){
         return text;
     }
     
+    /*
+     * This method returns the card's event type
+     * @author Ansley Solomon
+     * @return Returns the card's event type as a string
+     */    
     public String getEventType()
     {
         return event_type;
     }
     
+    /*
+     * This method returns the value that is a parameter for the event function
+     * @author Ansley Solomon
+     * @return Returns the card's event value as a string
+     */
     public String getEventValue()
     {
         return event_value;
     }
-        
-     
-    // Author: Sean Mitchel
-    // Get_Event
-    // Returns the event string from the member event object in a card
-    //
+
+    /*
+     * This method performs the event on the Player
+     * @author Sean Mitchell
+     * @param input The player that the event is happening to
+     * @return Returns the event string from the member event object in a card
+     */
     public void Get_Event(Player input)
     {
         if (this.getEventType().equals("collect"))
