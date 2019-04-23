89f00d926df0c59e2457d4368ae9ed835b0ae552
ansleysolo <33235644+ansleysolo@users.noreply.github.com>
2019-04-23 15:50:40 -0500

    Update Other_Tile.java

--- src/Other_Tile.java
+++ src/Other_Tile.java
@@ -1,38 +1,26 @@
 package monopolydriver;
 
+
 /**
-* Other_Tile.java
-*
-* This class contains the member variables and methods for "other tiles"
-* Other tiles are the two cost tiles on the board
-* These are the Buy Books and Pay Tutition tiles
-* 
-* @author Sean Mitchell
-*/
+ * This class contains the Other_Tile class. It is for the "Buy Books" and "Pay
+ * Tuition" tiles on the game board.
+ */
 public class Other_Tile 
 {
-    // ======================================
-    // Member variables
-    // ======================================
+    private int flag = 0;
     private String name;
     private String location;
     private String cost;
-    private String event_text;
-    private int flag;
     private boolean hasEvent = true;
     
-    // ======================================
-    // Constructors
-    // ======================================
     /**
-    * Constructor for Other_Tile types
-    *
-    * @author Sean Mitchell
-    * @param    String		name of tile
-    * @param	String		location of tile
-    * @param	String		cost of tile
-    * @return   Other_Tile    Other_Tile object
-    */
+     * Class constructor
+     * @author Sean Mitchell
+     * 
+     * @param name
+     * @param location
+     * @param cost 
+     */
     Other_Tile (String name, String location, String cost)
     {        
         this.name = name;
@@ -42,40 +30,29 @@ public class Other_Tile
 	switch(this.name)
 	{
 	    case "Buy Books":
-            {
 		this.flag = 1;
-                this.event_text = "Time to buy books! Pay $250";
 		break;
-            }
-	    case "Pay Tuition":
-            {
+	    case "Pay Tutition":
 		this.flag = 2;
-                this.event_text = "Time to pay tuition! Pay $250";
 		break;
-            }
-            default:
+	default:
 		break;
 	}
     }
     
-    // ======================================
-    // Accessors
-    // ======================================    
     /**
-     * Returns the name of the Property
-     * 
+     * Returns the name of the tile
      * @author Ansley Solomon
-     * @return String name
+     * @return Returns the name of the Property
      */
     public String getName(){
         return this.name;
     }
 
     /**
-     * Returns the Property's location on the Board
-     * 
+     * Returns the tile's location on the Board
      * @author Ansley Solomon
-     * @return int location
+     * @return Returns tile location on the game board
      */
     public int getLocation(){
         int lo = Integer.parseInt(location);	
@@ -83,10 +60,9 @@ public class Other_Tile
     }
 
     /**
-     * Returns the cost the player pays if they land on this tile
-     * 
-     * @author  Sean Mitchell
-     * @return  int  cost of landing on this tile
+     * Returns the tile's cost
+     * @author Ansley Solomon
+     * @return Returns the tile's cost
      */
     public int getCost()
     {
@@ -95,8 +71,7 @@ public class Other_Tile
     }
     
     /**
-     * Returns if the tile has an event
-     * 
+     * Tells if the tile has an event
      * @author Sean Mitchell
      * @return boolean hasEvent
      */
@@ -104,15 +79,13 @@ public class Other_Tile
     {
         return this.hasEvent;
     }
-    
+
     /**
-     * Returns the name of the Property
-     * 
-     * @author Ansley Solomon
-     * @return String name
+     * Returns the text that displays when the tile has an event
+     * @author Sean Mitchell
+     * @return String text
      */
-    public String getEventText()
-    {
-        return this.event_text;
+    String getEventText() {
+        return "no event";
     }
 }
